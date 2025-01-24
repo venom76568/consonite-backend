a17f1040a60cabbd691708dbd4cf1b058aff356b
venom76568 <hagrawal560@gmail.com>
2025-01-24 19:50:51 +0530

    cors url added

--- server.js
+++ server.js
@@ -9,7 +9,10 @@ const userRoutes = require("./routes/userRoutes");
 dotenv.config();
 
 const app = express();
-const allowedOrigins = ["localhost:5173", "https://your-frontend-domain.com"];
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://your-frontend-domain.com",
+];
 
 app.use(
   cors({
